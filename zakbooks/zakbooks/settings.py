16233fe8be6a88c713498687a4b19af23bff6d9e
zack-morpho <Zakaria.Karafi@emsi-edu.ma>
2025-05-16 23:17:18 +0100

    Remplacement mysqlclient par mysql-connector-python

--- zakbooks/zakbooks/settings.py
+++ zakbooks/zakbooks/settings.py
@@ -81,7 +81,13 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 WSGI_APPLICATION = 'zakbooks.wsgi.application'
 
