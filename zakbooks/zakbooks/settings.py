366db1e4488c1f4bdeaafa71685818e5f251d394
zack-morpho <Zakaria.Karafi@emsi-edu.ma>
2025-05-16 23:02:51 +0100

    Remplacement mysqlclient par mysql-connector-python

--- zakbooks/zakbooks/settings.py
+++ zakbooks/zakbooks/settings.py
@@ -28,13 +28,17 @@ SECRET_KEY = 'django-insecure-ot6*my2x4g8bcpa-tt4yq!)dwp7%rudmb@k)&+0l#=sd3jqf&(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'projet1-production-e398.up.railway.app',  # Domaine Railway
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
